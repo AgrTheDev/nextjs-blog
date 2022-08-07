dfd0b1f9c5cfb095642d5b5eeda10c60b85f2940
AGR <63412264+AGR-DEVS@users.noreply.github.com>
2022-08-07 11:47:13 -0400

    TypeScript Support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,29 @@
 import Head from "next/head"
 import Layout, { siteTitle } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
+import { getSortedPostsData } from "../lib/posts"
 import Link from "next/link"
 import Date from "../components/date"
+import { GetStaticProps } from "next"
 
-import { getSortedPostsData } from "../lib/posts"
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData,
-    },
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>This is a learning nextjs page</p>
+        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you'll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this in <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -45,4 +44,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
 }
